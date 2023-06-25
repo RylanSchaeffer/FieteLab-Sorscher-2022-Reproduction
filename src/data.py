Corrected in correct box_size factor of 2

src/data.py
@@ -34,9 +34,9 @@ class TrajectoryDataset(Dataset):
             for i in range(self.length):
                 generated_data = generate_trajectory(
                     batch_size=1,
-                    sequence_length=self.wandb_config['sequence_length'],
+                    sequence_length=self.wandb_config[f'sequence_length_{self.split}'],
                     dt=self.wandb_config['dt'],
-                    box_size=self.wandb_config['box_width_in_m'],
+                    box_size=self.wandb_config['box_width_in_m'] / 2.,
                 )
                 # Remove the batch dimension.
                 generated_data = {k: v[0] for k, v in generated_data.items()}
@@ -62,9 +62,9 @@ class TrajectoryDataset(Dataset):
             # Sample the data now.
             generated_data = generate_trajectory(
                 batch_size=1,
-                sequence_length=self.wandb_config['sequence_length'],
+                sequence_length=self.wandb_config[f'sequence_length_{self.split}'],
                 dt=self.wandb_config['dt'],
-                box_size=self.wandb_config['box_width_in_m'],
+                box_size=self.wandb_config['box_width_in_m'] / 2.,
             )
             item = {k: v[0].astype(np.float32) for k, v in generated_data.items()}
 
@@ -163,7 +163,7 @@ def generate_trajectory(batch_size: int,
     # initialize variables
     position = np.zeros((batch_size, sequence_length + 1, 2), dtype=float)
     head_dir = np.zeros((batch_size, sequence_length + 1), dtype=float)
-    turning = np.zeros((batch_size, sequence_length + 1), dtype='bool')
+    turning = np.zeros((batch_size, sequence_length + 1), dtype=bool)
     ego_speed = np.zeros((batch_size, sequence_length + 1), dtype=float)
 
     for batch_idx in range(batch_size):
@@ -172,14 +172,14 @@ def generate_trajectory(batch_size: int,
         head_dir[batch_idx, 0] = np.random.uniform(0, 2 * np.pi)
 
         # generate sequence of random boosts and turns
-        random_turn = np.random.normal(mu, sigma, sequence_length + 1)
-        random_vel = np.random.rayleigh(b, sequence_length + 1)
+        random_turn = np.random.normal(loc=mu, scale=sigma, size=sequence_length + 1)
+        random_vel = np.random.rayleigh(scale=b, size=sequence_length + 1)
 
         v = np.random.rayleigh(b)
         for i in range(1, sequence_length + 1):
             # If in border region, turn and slow down
             d_wall, a_wall = min_dist_angle(position[batch_idx, i - 1], head_dir[batch_idx, i - 1] % (2 * np.pi),
-                                            box_size)
+                                            box_size=box_size)
             if d_wall < border_region and np.abs(a_wall) < np.pi / 2:
                 turning[batch_idx, i - 1] = 1
                 turn_angle = np.sign(a_wall) * (np.pi / 2 - np.abs(a_wall)) + dt * random_turn[i]
@@ -197,7 +197,7 @@ def generate_trajectory(batch_size: int,
 
     ang_velocity = np.diff(head_dir, axis=1)
     theta_x, theta_y = np.cos(ang_velocity), np.sin(ang_velocity)
-    head_dir = (head_dir + np.pi) % (2 * np.pi) - np.pi  # Constrain head_dir to interval (-pi, pi)
+    head_dir = (head_dir + np.pi) % (2. * np.pi) - np.pi  # Constrain head_dir to interval (-pi, pi)
 
     # All arrays have shape (batch size = 1, appropriate temporal length, dim of variable)
     generated_data = {

src/systems.py
@@ -55,6 +55,9 @@ class GridCellSystem(pl.LightningModule):
                       batch: Dict[str, torch.Tensor],
                       batch_idx: int):
 
+        assert batch['target_pos'].min() >= (-self.wandb_config['box_width_in_m'] / 2.)
+        assert batch['target_pos'].max() <= (self.wandb_config['box_width_in_m'] / 2.)
+
         init_hd_values, init_pc_or_pos_values, recurrent_inputs = self.compute_inputs(batch=batch)
         forward_results = self.recurrent_network.forward(
             init_hd_values=init_hd_values,
@@ -77,13 +80,20 @@ class GridCellSystem(pl.LightningModule):
                      on_epoch=False,
                      sync_dist=True)
 
-        pos_decoding_err = self.compute_pos_decoding_err(
-            activations=forward_results['pc_logits'],
+        pos_decoding_err, pc_acc = self.compute_pos_decoding_err_and_acc(
+            pc_logits=forward_results['pc_logits'],
+            pc_or_pos_targets=pc_or_pos_targets,
             target_pos=batch['target_pos'],
         )
 
         self.log(f'train/pos_decoding_err',
                  pos_decoding_err,
+                 on_step=True,
+                 on_epoch=False,
+                 sync_dist=True)
+
+        self.log(f'train/pc_acc',
+                 pc_acc,
                  on_step=True,
                  on_epoch=False,
                  sync_dist=True)
@@ -116,13 +126,19 @@ class GridCellSystem(pl.LightningModule):
                      on_epoch=True,
                      sync_dist=True)
 
-        pos_decoding_err = self.compute_pos_decoding_err(
-            activations=forward_results['pc_logits'],
+        pos_decoding_err, pc_acc = self.compute_pos_decoding_err_and_acc(
+            pc_logits=forward_results['pc_logits'],
             target_pos=batch['target_pos'],
         )
 
         self.log(f'val/pos_decoding_err',
                  pos_decoding_err,
+                 on_step=False,
+                 on_epoch=True,
+                 sync_dist=True)
+
+        self.log(f'val/pc_acc',
+                 pc_acc,
                  on_step=False,
                  on_epoch=True,
                  sync_dist=True)
@@ -181,12 +197,16 @@ class GridCellSystem(pl.LightningModule):
 
         # Create initial conditions.
         with torch.no_grad():
+            # Shape: (batch size, 1, n_hd_cells)
             init_hd_values = self.hd_ensemble.get_init(batch['init_hd'])
             if self.wandb_config['hidden_state_init'] == 'pc_hd':
+                # Shape: (batch size, 1, n_pc_cells)
                 init_pc_or_pos_values = self.pc_ensemble.get_init(batch['init_pos'])
             else:
+                # Shape: (batch size, 1, 2)
                 init_pc_or_pos_values = batch['init_pos']
 
+            # Shape: (batch size, 1, 3)
             recurrent_inputs = torch.concat(
                 (batch['ego_speed'], batch['theta_x'], batch['theta_y']),
                 dim=2)
@@ -200,7 +220,7 @@ class GridCellSystem(pl.LightningModule):
                        hd_logits: torch.Tensor,
                        ) -> Dict[str, torch.Tensor]:
 
-        # Torch frustratingly requires the classes to be in the 1st dimension.
+        # Torch cross entropy frustratingly requires the classes to be in the 1st dimension.
         pc_loss = torch.mean(ce_loss_fn(
             input=pc_logits.transpose(1, 2).contiguous(),
             target=torch.argmax(pc_or_pos_targets, dim=2)
@@ -216,16 +236,17 @@ class GridCellSystem(pl.LightningModule):
         }
         return losses_results
 
-    def compute_pos_decoding_err(self,
-                                 activations: torch.Tensor,
-                                 target_pos: torch.Tensor,
-                                 num_top_pcs: int = 3,
-                                 ) -> torch.Tensor:
+    def compute_pos_decoding_err_and_acc(self,
+                                         pc_logits: torch.Tensor,
+                                         pc_or_pos_targets: torch.Tensor,
+                                         target_pos: torch.Tensor,
+                                         num_top_pcs: int = 3,
+                                         ) -> Tuple[torch.Tensor, torch.Tensor]:
 
-        _, top_k_indices = torch.topk(activations, k=num_top_pcs, dim=2)
+        _, top_k_indices = torch.topk(pc_logits, k=num_top_pcs, dim=2)
 
         # Make sure top_k_indices is of type long, as it's required for indexing
-        top_k_indices = top_k_indices.long()
+        # top_k_indices = top_k_indices.long()
 
         # Gathering using advanced indexing
         # Shape: (batch size, sequence length, num_top_pcs, spatial coordinates = 2)
@@ -233,12 +254,17 @@ class GridCellSystem(pl.LightningModule):
 
         # Average across the top three PCs.
         # Shape: (batch size, sequence length, 2)
-        pred_pos = torch.mean(gathered_means, dim=-2)
+        pred_pos = torch.mean(gathered_means, dim=2)
 
         # Multiplying by 100 converts from meters to centimeters.
-        pos_decoding_err = 100 * torch.mean(torch.linalg.norm(pred_pos - target_pos, dim=2))
+        pos_decoding_err = 100. * torch.mean(torch.linalg.norm(pred_pos - target_pos, dim=2))
 
-        return pos_decoding_err
+        pc_acc = torch.mean(
+            torch.eq(torch.argmax(pc_logits, dim=2),
+                     torch.argmax(pc_or_pos_targets, dim=2)).float()
+        )
+
+        return pos_decoding_err, pc_acc
 
     def compute_targets(self,
                         batch: Dict[str, torch.Tensor]):
@@ -400,12 +426,7 @@ class SorscherRecurrentNetwork(pl.LightningModule):
 
         # Compute initial LSTM hidden state values.
         init_hd_activations = self.hd_init_layer(init_hd_values)
-        if self.wandb_config['hidden_state_init'] == 'pc_hd':
-            init_pc_or_pos_activations = self.pc_or_pos_init_layer(init_pc_or_pos_values)
-        elif self.wandb_config['hidden_state_init'] == 'pos_hd':
-            init_pc_or_pos_activations = self.pc_or_pos_init_layer(init_pc_or_pos_values)
-        else:
-            raise ValueError
+        init_pc_or_pos_activations = self.pc_or_pos_init_layer(init_pc_or_pos_values)
         added_init_activations = init_pc_or_pos_activations + init_hd_activations
 
         # We need to swap batch & time dimension ourselves.